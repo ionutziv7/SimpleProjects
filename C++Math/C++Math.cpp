92bbfaa06096fcbcb50f37911299aa464d67de0a
John I <93291968+ionutziv7@users.noreply.github.com>
2022-03-05 21:33:10 +0200

    Create C++Math.cpp

---
+++ C++Math/C++Math.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cmath>
+ 
+using namespace std;
+
+int main()
+{
+    int x = 5;
+    int y = 10;
+    
+    cout << max(x, y)<<endl;
+    cout << min(x, y) << endl;
+    cout<<sqrt(49)<<endl;
+    cout<<round(76.7)<<endl;
+    
+	return 0;
+}
