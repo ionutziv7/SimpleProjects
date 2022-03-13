9a4486576dc6b363e6ed1a2ff5d22480903bff7d
John I <93291968+ionutziv7@users.noreply.github.com>
2022-03-13 18:24:40 +0200

    Create ForLoops.cpp

---
+++ For Loops/ForLoops.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    for(int i = 1; i <= 5; i++)
+        cout << i << " "; 
+    cout <<endl<<endl;
+    
+    for(int i = 10; i<= 20; i++)
+        cout << i <<" ";
+     cout <<endl<<endl;
+     
+     int n1 = 17, n2 = 26;
+     for(int i = n1; i <= n2; i++)
+         cout << i << " ";
+     cout <<endl<<endl;
+     
+     for(int i = 10; i >= 1; i--)
+            cout << i << " ";
+     cout <<endl<<endl;
+     
+     for( int i = 1; i <=11; i = i + 2)
+            cout << i << " ";
+     cout <<endl<<endl;
+     
+     for(int j = 1; j <=5; j++)
+         cout << j * 2 << " ";
+    cout <<endl<<endl;
+    
+     for(int i=1; i<=3; i++) {
+     for(int j = 1; j<=7; j++) {
+            cout << i << " * " << j << " = " << i * j<<endl;
+        }
+        cout<<endl;
+    }
+	return 0;
+}
