414e545658a12e3597da97fedc8321e75507423f
John I <93291968+ionutziv7@users.noreply.github.com>
2022-03-13 17:15:35 +0200

    Update ConvertTemperatures.cpp

--- ConvertTemperatures/ConvertTemperatures.cpp
+++ ConvertTemperatures/ConvertTemperatures.cpp
@@ -3,12 +3,17 @@ using namespace std;
 
 int main()
 {
-    double ctemp, ftemp;
+    double ctemp;   //Celsius temperature
+    
+    //Prompt and input value of ctemp.
     
     cout <<"Input a Celsius temp and press ENTER: ";
     cin >> ctemp;
-    ftemp = (ctemp * 1.8) + 32;
-    cout << "Fahrenheit temp is: " << ftemp << endl;
+    
+    //Convert ctemp and output results.
+
+    cout << "Fahrenheit temp is: " << (ctemp * 1.8) + 32;
+    cout << endl;
     
     system("PAUSE");
 	return 0;
