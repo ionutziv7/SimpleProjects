a0ec9c3716d92f35102c86bbac4a53fc89519835
John I <93291968+ionutziv7@users.noreply.github.com>
2022-03-15 21:33:50 +0200

    Update Calculator.h

--- Calculator_v2.0/Calculator.h
+++ Calculator_v2.0/Calculator.h
@@ -5,15 +5,19 @@ class Calculator
 public:
 	void Display();
 	void Input();
+	void DoubleInput();
+	void LongInput();
 	void Addition();
 	void Substraction();
 	void Multiplication();
 	void Division();
+	void DivisionWithoutRemainder();
 	void Modulo();
 	void Sqrt();
 	void Pow();
 	int number1 = 0, number2 = 0;
 	double dnumber1 = 0, dnumber2 = 0;
+	long long lnumber1 = 0, lnumber2 = 0;
 	Calculator()
 	{
 		std::cout << "Calculator v2.0" << std::endl;
